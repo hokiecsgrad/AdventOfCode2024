1660ad8d6664f02868e4137fe9de57704a2de4c8
Ryan Hagan <ryan@ryanhagan.net>
2024-12-04 11:45:24 -0500

    Solved Day 4

--- Day04/src/Solver.cs
+++ Day04/src/Solver.cs
@@ -6,13 +6,112 @@ public class Solver
 {
     public void SolvePart1(string[] data)
     {
+        int total = 0;
 
-        Console.WriteLine($"Part 1: ");
+        char[,] grid = CreateGrid(data);
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+            for (int x = 0; x < grid.GetLength(1); x++)
+                if (grid[y, x] == 'X')
+                {
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            if (IsMas(grid, (y + i, x + j), (i, j)))
+                                total++;
+                }
+        Console.WriteLine($"Part 1: {total}");
+    }
+
+    public char[,] CreateGrid(string[] data)
+    {
+        int height = data.Length;
+        int width = data[0].ToCharArray().Length;
+
+        char[,] grid = new char[height, width];
+
+        for (int y = 0; y < height; y++)
+        {
+            char[] row = data[y].ToCharArray();
+            for (int x = 0; x < width; x++)
+                grid[y, x] = row[x];
+        }
+
+        return grid;
+    }
+
+    public (int, int)[] GetNeighborsThatAre(char target, char[,] grid, int y, int x)
+    {
+        List<(int, int)> neighbors = new();
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++)
+                if (
+                    y + i >= 0 &&
+                    y + i < grid.GetLength(0) &&
+                    x + j >= 0 &&
+                    x + j < grid.GetLength(1) &&
+                    grid[y + i, x + j] == target
+                    )
+                    neighbors.Add((i, j));
+
+        return neighbors.ToArray();
     }
 
     public void SolvePart2(string[] data)
     {
+        int total = 0;
 
-        Console.WriteLine($"Part 2: ");
+        char[,] grid = CreateGrid(data);
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+            for (int x = 0; x < grid.GetLength(1); x++)
+                if (grid[y, x] == 'M')
+                {
+                    if (IsMas(grid, (y, x), (1, 1)) &&
+                        IsMas(grid, (y, x + 2), (1, -1)))
+                        total++;
+                    else if (IsMas(grid, (y, x), (1, 1)) &&
+                        IsSam(grid, (y, x + 2), (1, -1)))
+                        total++;
+                }
+                else if (grid[y, x] == 'S')
+                {
+                    if (IsSam(grid, (y, x), (1, 1)) &&
+                        IsMas(grid, (y, x + 2), (1, -1)))
+                        total++;
+                    else if (IsSam(grid, (y, x), (1, 1)) &&
+                        IsSam(grid, (y, x + 2), (1, -1)))
+                        total++;
+                }
+        Console.WriteLine($"Part 2: {total}");
+    }
+
+    public bool IsMas(char[,] grid, (int y, int x) pos, (int i, int j) dir)
+    {
+        if (pos.y + (dir.i * 2) >= 0 &&
+            pos.x + (dir.j * 2) >= 0 &&
+            pos.y + (dir.i * 2) < grid.GetLength(0) &&
+            pos.x + (dir.j * 2) < grid.GetLength(1) &&
+            grid[pos.y, pos.x] == 'M' &&
+            grid[pos.y + dir.i, pos.x + dir.j] == 'A' &&
+            grid[pos.y + (dir.i * 2), pos.x + (dir.j * 2)] == 'S'
+        )
+            return true;
+        else
+            return false;
+    }
+
+    public bool IsSam(char[,] grid, (int y, int x) pos, (int i, int j) dir)
+    {
+        if (pos.y + (dir.i * 2) >= 0 &&
+            pos.x + (dir.j * 2) >= 0 &&
+            pos.y + (dir.i * 2) < grid.GetLength(0) &&
+            pos.x + (dir.j * 2) < grid.GetLength(1) &&
+            grid[pos.y, pos.x] == 'S' &&
+            grid[pos.y + dir.i, pos.x + dir.j] == 'A' &&
+            grid[pos.y + (dir.i * 2), pos.x + (dir.j * 2)] == 'M'
+        )
+            return true;
+        else
+            return false;
     }
 }
--- Day04/tst/Day04Tests.cs
+++ Day04/tst/Day04Tests.cs
@@ -8,27 +8,130 @@ public class Day04Tests
 {
     public Day04Tests()
     {
-        data = sampleInput.Split(
+    }
+
+    [Fact]
+    public void Part1_WithSimpleData_ShouldBe4()
+    {
+        data = simpleInputPart1.Split(
             '\n',
             StringSplitOptions.TrimEntries |
             StringSplitOptions.RemoveEmptyEntries
             );
+
+        Solver solver = new();
+        int total = 0;
+
+        char[,] grid = solver.CreateGrid(data);
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+            for (int x = 0; x < grid.GetLength(1); x++)
+                if (grid[y, x] == 'X')
+                {
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            if (solver.IsMas(grid, (y + i, x + j), (i, j)))
+                                total++;
+                }
+
+        Assert.Equal(4, total);
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void Part1_WithSampleData_ShouldBe18()
     {
-        throw new NotImplementedException();
+        data = sampleInputPart1.Split(
+            '\n',
+            StringSplitOptions.TrimEntries |
+            StringSplitOptions.RemoveEmptyEntries
+            );
+
+        Solver solver = new();
+        int total = 0;
+
+        char[,] grid = solver.CreateGrid(data);
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+            for (int x = 0; x < grid.GetLength(1); x++)
+                if (grid[y, x] == 'X')
+                {
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            if (solver.IsMas(grid, (y + i, x + j), (i, j)))
+                                total++;
+                }
+
+        Assert.Equal(18, total);
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void Part2_WithSampleData_ShouldBe9()
     {
-        throw new NotImplementedException();
+        data = sampleInputPart2.Split(
+            '\n',
+            StringSplitOptions.TrimEntries |
+            StringSplitOptions.RemoveEmptyEntries
+            );
+
+        Solver solver = new();
+        int total = 0;
+
+        char[,] grid = solver.CreateGrid(data);
+
+        for (int y = 0; y < grid.GetLength(0); y++)
+            for (int x = 0; x < grid.GetLength(1); x++)
+                if (grid[y, x] == 'M')
+                {
+                    if (solver.IsMas(grid, (y, x), (1, 1)) &&
+                        solver.IsMas(grid, (y, x + 2), (1, -1)))
+                        total++;
+                    else if (solver.IsMas(grid, (y, x), (1, 1)) &&
+                        solver.IsSam(grid, (y, x + 2), (1, -1)))
+                        total++;
+                }
+                else if (grid[y, x] == 'S')
+                {
+                    if (solver.IsSam(grid, (y, x), (1, 1)) &&
+                        solver.IsMas(grid, (y, x + 2), (1, -1)))
+                        total++;
+                    else if (solver.IsSam(grid, (y, x), (1, 1)) &&
+                        solver.IsSam(grid, (y, x + 2), (1, -1)))
+                        total++;
+                }
+
+        Assert.Equal(9, total);
     }
 
     private string[] data;
-    private string sampleInput = """
-SAMPLE INPUT HERE
+    private string simpleInputPart1 = """
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....
+""";
+    private string sampleInputPart1 = """
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+""";
+    private string sampleInputPart2 = """
+.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........
 """;
 }
