22ffcc0c70b219edaffcb671676047ffa42cbe8d
Ryan Hagan <ryan@ryanhagan.net>
2024-12-05 12:53:26 -0500

    Solved Day 5

--- Day05/src/Solver.cs
+++ Day05/src/Solver.cs
@@ -6,11 +6,173 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        int total = 0;
+
+        (Dictionary<int, HashSet<int>> before, Dictionary<int, HashSet<int>> after)
+            rules = GetRules(data);
+        List<List<int>> pageNums = GetPageNums(data);
+
+        (List<int> validUpdates, List<int> invalidUpdates) =
+            GetIndexesOfUpdates(
+                rules.before,
+                rules.after,
+                pageNums);
+
+        foreach (int index in validUpdates)
+        {
+            int len = pageNums[index].Count();
+            total += pageNums[index][len / 2];
+        }
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        int total = 0;
+
+        (Dictionary<int, HashSet<int>> before, Dictionary<int, HashSet<int>> after)
+            rules = GetRules(data);
+        List<List<int>> pageNums = GetPageNums(data);
+
+        (List<int> validUpdates, List<int> invalidUpdates) =
+            GetIndexesOfUpdates(
+                rules.before,
+                rules.after,
+                pageNums);
+
+        foreach (int index in invalidUpdates)
+            pageNums[index] = FixPageUpdates(pageNums[index], rules.before, rules.after);
+
+        foreach (int index in invalidUpdates)
+        {
+            int len = pageNums[index].Count();
+            total += pageNums[index][len / 2];
+        }
+
+        return total.ToString();
+    }
+
+    public (Dictionary<int, HashSet<int>>, Dictionary<int, HashSet<int>>) GetRules(string[] data)
+    {
+        Dictionary<int, HashSet<int>> beforeRules = new();
+        Dictionary<int, HashSet<int>> afterRules = new();
+
+        int index = 0;
+        while (data[index] != "")
+        {
+            int[] currRule = data[index].Split('|').Select(int.Parse).ToArray();
+            if (afterRules.ContainsKey(currRule[0]))
+                afterRules[currRule[0]].Add(currRule[1]);
+            else
+                afterRules.Add(currRule[0], new HashSet<int> { currRule[1] });
+
+            if (beforeRules.ContainsKey(currRule[1]))
+                beforeRules[currRule[1]].Add(currRule[0]);
+            else
+                beforeRules.Add(currRule[1], new HashSet<int> { currRule[0] });
+
+            index++;
+        }
+
+        return (beforeRules, afterRules);
+    }
+
+    public List<List<int>> GetPageNums(string[] data)
+    {
+        List<List<int>> pageNums = new();
+
+        int index = 0;
+        while (data[index] != "")
+            index++;
+
+        for (index++; index < data.Length; index++)
+        {
+            List<int> currUpdate = data[index].Split(',').Select(int.Parse).ToList();
+            pageNums.Add(currUpdate);
+        }
+
+        return pageNums;
+    }
+
+    public (List<int>, List<int>) GetIndexesOfUpdates(
+        Dictionary<int, HashSet<int>> beforeRules,
+        Dictionary<int, HashSet<int>> afterRules,
+        List<List<int>> pageNums)
+    {
+        List<int> validUpdates = new();
+        List<int> invalidUpdates = new();
+
+        for (int i = 0; i < pageNums.Count(); i++)
+            if (GetInvalidNumAndRule(pageNums[i], beforeRules, afterRules) is null)
+                validUpdates.Add(i);
+            else
+                invalidUpdates.Add(i);
+
+        return (validUpdates, invalidUpdates);
+    }
+
+    public (int, HashSet<int>, HashSet<int>)? GetInvalidNumAndRule(
+        List<int> pageNums,
+        Dictionary<int, HashSet<int>> beforeRules,
+        Dictionary<int, HashSet<int>> afterRules)
+    {
+        for (int j = 0; j < pageNums.Count(); j++)
+        {
+            int currNum = pageNums[j];
+            // put all nums before current num in a "before set"
+            HashSet<int> befores = new HashSet<int>(pageNums.Take(j));
+            // put all nums after current num in an "after set"
+            HashSet<int> afters = new HashSet<int>(pageNums.Skip(j + 1));
+            // make sure curr num after set doesn't contain anything in the before set
+            if (beforeRules.ContainsKey(currNum) && afters.Overlaps(beforeRules[currNum]))
+            {
+                return (currNum, beforeRules[currNum], new HashSet<int>());
+            }
+            // Make sure curr num before set doesn't contain anything in the after set
+            if (afterRules.ContainsKey(currNum) && befores.Overlaps(afterRules[currNum]))
+            {
+                return (currNum, new HashSet<int>(), afterRules[currNum]);
+            }
+        }
+        return null;
+    }
+
+    public List<int> FixPageUpdates(
+        List<int> updates,
+        Dictionary<int, HashSet<int>> beforeRules,
+        Dictionary<int, HashSet<int>> afterRules)
+    {
+        List<int> fixedUpdates = updates;
+
+        (int num, HashSet<int> beforeRule, HashSet<int> afterRule)? update =
+            GetInvalidNumAndRule(updates, beforeRules, afterRules);
+
+        while (update is not null)
+        {
+            int indexOfNum = updates.IndexOf(update.Value.num);
+            int indexOfBadValue = -1;
+            int badValue = -1;
+
+            if (update is not null && update.Value.beforeRule.Count > 0)
+            {
+                HashSet<int> afters = new HashSet<int>(fixedUpdates.Skip(indexOfNum + 1));
+                badValue = afters.Intersect(update.Value.beforeRule).ToList().First();
+                indexOfBadValue = fixedUpdates.IndexOf(badValue);
+            }
+            else if (update is not null && update.Value.afterRule.Count > 0)
+            {
+                HashSet<int> befores = new HashSet<int>(fixedUpdates.Take(indexOfNum - 1));
+                badValue = befores.Intersect(update.Value.afterRule).ToList().First();
+                indexOfBadValue = fixedUpdates.IndexOf(badValue);
+            }
+
+            fixedUpdates[indexOfNum] = badValue;
+            fixedUpdates[indexOfBadValue] = update.Value.num;
+
+            update = GetInvalidNumAndRule(fixedUpdates, beforeRules, afterRules);
+        }
+
+        return fixedUpdates;
     }
 }
--- Day05/tst/Day05Tests.cs
+++ Day05/tst/Day05Tests.cs
@@ -10,25 +10,83 @@ public class Day05Tests
     {
         data = sampleInput.Split(
             '\n',
-            StringSplitOptions.TrimEntries |
-            StringSplitOptions.RemoveEmptyEntries
+            StringSplitOptions.TrimEntries
             );
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void GetRules_WithSampleData_ShouldReturnDictionariesOfBeforeAndAfterRules()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+
+        (Dictionary<int, HashSet<int>> before, Dictionary<int, HashSet<int>> after)
+            rules = solver.GetRules(data);
+
+        Assert.Equal(rules.before[47], new HashSet<int> { 97, 75 });
+        Assert.Equal(rules.after[61], new HashSet<int> { 13, 53, 29 });
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void GetPageNums_WithSampleData_ShouldReturn6Updates()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+
+        List<List<int>> pageNums = solver.GetPageNums(data);
+
+        Assert.Equal(6, pageNums.Count());
+        Assert.Equal(3, pageNums[2].Count());
+        Assert.Equal(5, pageNums[5].Count());
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe143()
+    {
+        Solver solver = new();
+
+        int total = int.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(143, total);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe123()
+    {
+        Solver solver = new();
+
+        int total = int.Parse(solver.SolvePart2(data));
+
+        Assert.Equal(123, total);
     }
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
 """;
 }
