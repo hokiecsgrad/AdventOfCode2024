125556b9bd78cf7871bbbf924eda0a026cb1d4ed
Ryan Hagan <ryan@ryanhagan.net>
2024-12-07 11:01:47 -0500

    Solved for Day 7

--- Day07/src/Solver.cs
+++ Day07/src/Solver.cs
@@ -6,11 +6,130 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        long total = 0;
+        List<(long, List<int>)> input = ParseData(data);
+
+        foreach ((long target, List<int> nums) line in input)
+        {
+            Tree numTree = BuildTree(line.nums, new List<char> { '+', '*' });
+            total += EvalTreeForTarget(numTree, line.target);
+        }
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        long total = 0;
+        List<(long, List<int>)> input = ParseData(data);
+
+        foreach ((long target, List<int> nums) line in input)
+        {
+            Tree numTree = BuildTree(line.nums, new List<char> { '+', '*', '|' });
+            total += EvalTreeForTarget(numTree, line.target);
+        }
+
+        return total.ToString();
     }
+
+    public Tree BuildTree(List<int> nums, List<char> ops)
+    {
+        Tree opsTree = new();
+
+        List<Node> currNodes = new();
+        currNodes.Add(new Node() { Value = nums[0] });
+        for (int i = 0; i < nums.Count() - 1; i++)
+        {
+            List<Node> processedNodes = new();
+            for (int nIndex = 0; nIndex < currNodes.Count(); nIndex++)
+            {
+                Node currNode = currNodes[nIndex];
+                foreach (char op in ops)
+                {
+                    Edge edge = new Edge()
+                    {
+                        Op = op,
+                        Source = currNode,
+                        Dest = new Node() { Value = nums[i + 1] }
+                    };
+                    currNode.Edges.Add(edge);
+                    processedNodes.Add(edge.Dest);
+                }
+            }
+            if (i == 0) opsTree.Root = currNodes[0];
+            currNodes = processedNodes;
+        }
+
+        return opsTree;
+    }
+
+    public long EvalTreeForTarget(Tree tree, long target)
+    {
+        return EvalNode(tree.Root, target, 0);
+    }
+
+    private long EvalNode(Node node, long target, long accumulator)
+    {
+        if (node.Edges.Count == 0 && accumulator == target) return accumulator;
+        if (node.Edges.Count == 0) return 0;
+
+        if (accumulator == 0) accumulator = node.Value;
+
+        foreach (Edge edge in node.Edges)
+        {
+            long total = 0;
+            total = edge.Op switch
+            {
+                '+' => EvalNode(edge.Dest, target, accumulator + edge.Dest.Value),
+                '*' => EvalNode(edge.Dest, target, accumulator * edge.Dest.Value),
+                '|' => EvalNode(edge.Dest, target, Concat(accumulator, edge.Dest.Value)),
+            };
+            if (total == target) return total;
+        }
+        return 0;
+    }
+
+    public long Concat(long x, long y)
+    {
+        for (var z = y; z > 0; z /= 10)
+            x *= 10;
+        return x + y;
+    }
+
+    public List<(long, List<int>)> ParseData(string[] data)
+    {
+        List<(long, List<int>)> input = new();
+
+        foreach (string line in data)
+        {
+            long total = long.Parse(line.Split(':')[0]);
+            List<int> nums = line
+                .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .Select(int.Parse)
+                .ToList();
+            input.Add((total, nums));
+        }
+
+        return input;
+    }
+}
+
+public class Tree
+{
+    public Node? Root { get; set; }
+
+}
+
+public class Node
+{
+    public int Value { get; set; } = 0;
+    public List<Edge> Edges { get; set; } = new();
+}
+
+public class Edge
+{
+    public char Op { get; set; }
+    public Node Source { get; set; }
+    public Node Dest { get; set; }
 }
--- Day07/tst/Day07Tests.cs
+++ Day07/tst/Day07Tests.cs
@@ -16,19 +16,42 @@ public class Day07Tests
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void ParseData_WithSampleData_ShouldBeListOfLists()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        List<(long, List<int>)> input = solver.ParseData(data);
+
+        Assert.Equal(9, input.Count());
+        Assert.Equal(7290, input[4].Item1);
+        Assert.Equal(6, input[4].Item2[2]);
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void Part1_WithSampleData_ShouldBe3749()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        long total = int.Parse(solver.SolvePart1(data));
+        Assert.Equal(3749, total);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe11387()
+    {
+        Solver solver = new();
+        long total = int.Parse(solver.SolvePart2(data));
+        Assert.Equal(11387, total);
     }
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
 """;
 }
