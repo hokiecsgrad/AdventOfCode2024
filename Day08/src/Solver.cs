b2598d90af7093429bd49f24fc62c5adf7ef3e28
Ryan Hagan <ryan@ryanhagan.net>
2024-12-08 10:24:52 -0500

    Solved Day 8

--- Day08/src/Solver.cs
+++ Day08/src/Solver.cs
@@ -1,16 +1,165 @@
+using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Day08;
 
 public class Solver
 {
+    private Regex AntPattern = new Regex(@"^[0-9a-zA-Z]{1}$");
+
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        int total = 0;
+        char[,] antiGrid = new char[data.Length, data[0].Length];
+
+        HashSet<char> ants = GetUniqueAntennas(data);
+        char[,] currGrid;
+        foreach (char antenna in ants)
+        {
+            currGrid = CreateGridWithAntenna(data, antenna);
+            List<(Point, Point)> pairs = GetAntennaPairs(currGrid);
+            foreach ((Point a, Point b) pair in pairs)
+            {
+                Vector antinode = (pair.a - pair.b) * 2;
+                if (
+                        pair.a.Row + antinode.Y >= 0 &&
+                        pair.a.Col + antinode.X >= 0 &&
+                        pair.a.Row + antinode.Y < antiGrid.GetLength(0) &&
+                        pair.a.Col + antinode.X < antiGrid.GetLength(1))
+                    antiGrid[pair.a.Row + antinode.Y, pair.a.Col + antinode.X] = '#';
+            }
+        }
+
+        for (int row = 0; row < antiGrid.GetLength(0); row++)
+            for (int col = 0; col < antiGrid.GetLength(1); col++)
+                if (antiGrid[row, col] == '#')
+                    total++;
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        int total = 0;
+        char[,] antiGrid = new char[data.Length, data[0].Length];
+        FillGrid(antiGrid, '.');
+
+        HashSet<char> ants = GetUniqueAntennas(data);
+        char[,] currGrid;
+        foreach (char antenna in ants)
+        {
+            currGrid = CreateGridWithAntenna(data, antenna);
+            List<(Point, Point)> pairs = GetAntennaPairs(currGrid);
+            foreach ((Point a, Point b) pair in pairs)
+            {
+                int mult = 1;
+                Vector antinode = (pair.a - pair.b);
+                Point nextAntinodePoint = new Point()
+                {
+                    Row = pair.a.Row + (antinode * mult).Y,
+                    Col = pair.a.Col + (antinode * mult).X
+                };
+                while (IsInBounds(antiGrid, nextAntinodePoint))
+                {
+                    antiGrid[nextAntinodePoint.Row, nextAntinodePoint.Col] = '#';
+                    mult++;
+                    nextAntinodePoint = new Point()
+                    {
+                        Row = pair.a.Row + (antinode * mult).Y,
+                        Col = pair.a.Col + (antinode * mult).X
+                    };
+                }
+            }
+        }
+
+        for (int row = 0; row < antiGrid.GetLength(0); row++)
+            for (int col = 0; col < antiGrid.GetLength(1); col++)
+                if (antiGrid[row, col] == '#')
+                    total++;
+
+        return total.ToString();
     }
+
+    public bool IsInBounds(char[,] grid, Point point)
+        => point.Row >= 0 &&
+            point.Row < grid.GetLength(0) &&
+            point.Col >= 0 &&
+            point.Col < grid.GetLength(1);
+
+    public List<(Point, Point)> GetAntennaPairs(char[,] currGrid)
+    {
+        List<(Point, Point)> pairs = new();
+        List<Point> antenna = new();
+
+        for (int row = 0; row < currGrid.GetLength(0); row++)
+            for (int col = 0; col < currGrid.GetLength(1); col++)
+                if (currGrid[row, col] != '.')
+                    antenna.Add(new Point() { Row = row, Col = col });
+
+        for (int i = 0; i < antenna.Count(); i++)
+            for (int j = 0; j < antenna.Count(); j++)
+                if (i != j) pairs.Add((antenna[i], antenna[j]));
+
+        return pairs;
+    }
+
+    public HashSet<char> GetUniqueAntennas(string[] data)
+    {
+        HashSet<char> ants = new();
+        for (int row = 0; row < data.Length; row++)
+            for (int col = 0; col < data[row].Length; col++)
+                if (AntPattern.IsMatch(data[row][col].ToString()))
+                    if (!ants.Contains(data[row][col]))
+                        ants.Add(data[row][col]);
+        return ants;
+    }
+
+    public char[,] CreateGridWithAntenna(string[] data, char antenna)
+    {
+        char[,] grid = new char[data.Length, data[0].Length];
+
+        for (int row = 0; row < data.Length; row++)
+            for (int col = 0; col < data[row].Length; col++)
+                if (data[row][col] == antenna)
+                    grid[row, col] = data[row][col];
+                else
+                    grid[row, col] = '.';
+
+        return grid;
+    }
+
+    public void FillGrid(char[,] grid, char fill)
+    {
+        for (int row = 0; row < grid.GetLength(0); row++)
+            for (int col = 0; col < grid.GetLength(1); col++)
+                grid[row, col] = fill;
+    }
+
+    public void PrintGrid(char[,] grid)
+    {
+        for (int row = 0; row < grid.GetLength(0); row++)
+        {
+            for (int col = 0; col < grid.GetLength(1); col++)
+                Console.Write(grid[row, col]);
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Point
+{
+    public int Row { get; set; } = 0;
+    public int Col { get; set; } = 0;
+
+    public static Vector operator -(Point a, Point b)
+        => new Vector() { Y = b.Row - a.Row, X = b.Col - a.Col };
+}
+
+public record Vector
+{
+    public int Y { get; set; } = 0;
+    public int X { get; set; } = 0;
+
+    public static Vector operator *(Vector a, int scale)
+        => new Vector() { Y = a.Y * scale, X = a.X * scale };
 }
--- Day08/tst/Day08Tests.cs
+++ Day08/tst/Day08Tests.cs
@@ -16,19 +16,74 @@ public class Day08Tests
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void GetAntennas_WithSampleData_ShouldReturn0A()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        HashSet<char> ants = solver.GetUniqueAntennas(data);
+        Assert.Equal(2, ants.Count());
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void CreateGrid_FromSampleData0_ShouldReturnGridWithOnly0()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        char[,] grid = solver.CreateGridWithAntenna(data, '0');
+        bool gridIsOnly0 = true;
+        for (int row = 0; row < grid.GetLength(0); row++)
+            for (int col = 0; col < grid.GetLength(1); col++)
+                if (grid[row, col] == '.' || grid[row, col] == '0')
+                    gridIsOnly0 = true;
+                else
+                    gridIsOnly0 = false;
+
+        Assert.True(gridIsOnly0);
+    }
+
+    [Fact]
+    public void GetPairs_FromSampleData0_ShouldReturn12()
+    {
+        Solver solver = new();
+        char[,] grid = solver.CreateGridWithAntenna(data, '0');
+        List<(Point, Point)> pairs = solver.GetAntennaPairs(grid);
+
+        Assert.Equal(12, pairs.Count());
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe14()
+    {
+        int total = 0;
+        Solver solver = new();
+
+        total = int.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(14, total);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe34()
+    {
+        int total = 0;
+        Solver solver = new();
+
+        total = int.Parse(solver.SolvePart2(data));
+
+        Assert.Equal(34, total);
     }
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
 """;
 }
