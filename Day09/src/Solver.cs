744dd6fda6c86e271bcdbbe479a28bfeb5ba0ab1
Ryan Hagan <ryan@ryanhagan.net>
2024-12-10 08:04:07 -0500

    Solved Day 9 Part 1, but not part 2

--- Day09/src/Solver.cs
+++ Day09/src/Solver.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Day09;
@@ -6,11 +7,121 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        long checksum = 0;
+
+        List<long> hdd = ExplodeMap(data[0]);
+        hdd = Defrag(hdd);
+        checksum = CalcChecksum(hdd);
+
+        return checksum.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        long checksum = 0;
+
+        List<long> hdd = ExplodeMap(data[0]);
+        hdd = DefragContiguous(hdd);
+        checksum = CalcChecksum(hdd);
+
+        return checksum.ToString();
+    }
+
+    public List<long> ExplodeMap(string map)
+    {
+        List<long> hdd = new();
+        int fileId = 0;
+        for (int i = 0; i < map.Length; i += 2)
+        {
+            int blocksize = int.Parse(map[i].ToString());
+            for (int j = 0; j < blocksize; j++)
+                hdd.Add(fileId);
+            if (i + 1 < map.Length)
+            {
+                int freespace = int.Parse(map[i + 1].ToString());
+                for (int j = 0; j < freespace; j++)
+                    hdd.Add(-1);
+            }
+            fileId++;
+        }
+
+        return hdd;
+    }
+
+    public List<long> Defrag(List<long> hdd)
+    {
+        List<long> defragged = new List<long>(hdd);
+        int lIndex = 0;
+        int rIndex = defragged.Count() - 1;
+
+        while (lIndex < rIndex)
+        {
+            // move left index forward until it hits free space
+            while (defragged[lIndex] != -1)
+                lIndex++;
+            // move right index backwards until it hits data
+            while (defragged[rIndex] == -1)
+                rIndex--;
+            if (lIndex < rIndex)
+            {
+                defragged[lIndex] = defragged[rIndex];
+                defragged[rIndex] = -1;
+            }
+        }
+
+        return defragged;
+    }
+
+    public List<long> DefragContiguous(List<long> hdd)
+    {
+        List<long> defragged = new List<long>(hdd);
+        long currFileId = hdd.Max();
+        int rIndex = defragged.Count() - 1;
+
+        while (currFileId > 0)
+        {
+            // get info about rightmost file
+            while (defragged[rIndex] != currFileId) rIndex--;
+            int endFile = rIndex;
+            while (defragged[rIndex] == currFileId) rIndex--;
+            int startFile = rIndex + 1;
+            int fileLength = (endFile - startFile) + 1;
+
+            bool spaceFound = false;
+            int lIndex = 0;
+            // now keep testing space on the left until there's no more space to test
+            while (!spaceFound && lIndex < rIndex)
+            {
+                while (defragged[lIndex] != -1) lIndex++;
+                int startFreespace = lIndex;
+                while (defragged[lIndex] == -1) lIndex++;
+                int endFreespace = lIndex;
+                int amtFreespace = endFreespace - startFreespace;
+
+                if (amtFreespace >= fileLength && lIndex < rIndex)
+                {
+                    spaceFound = true;
+                    for (int i = 0; i < fileLength; i++)
+                    {
+                        defragged[i + startFreespace] = currFileId;
+                        defragged[i + startFile] = -1;
+                    }
+                }
+            }
+            currFileId--;
+        }
+
+        return defragged;
+    }
+
+    public long CalcChecksum(List<long> hdd)
+    {
+        long total = 0;
+
+        for (int i = 0; i < hdd.Count(); i++)
+            if (hdd[i] != -1)
+                total += (i * hdd[i]);
+
+        return total;
     }
 }
--- Day09/tst/Day09Tests.cs
+++ Day09/tst/Day09Tests.cs
@@ -16,19 +16,109 @@ public class Day09Tests
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void ExplodeMap_WithSampleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        List<long> expected = "00...111...2...333.44.5555.6666.777.888899"
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        List<long> hdd = solver.ExplodeMap(data[0]);
+        Assert.Equal(expected, hdd);
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void Defrag_WithSampleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        List<long> hdd = "00...111...2...333.44.5555.6666.777.888899"
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        List<long> expected = "0099811188827773336446555566.............."
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        hdd = solver.Defrag(hdd);
+
+        Assert.Equal(expected, hdd);
+    }
+
+    [Fact]
+    public void CalcChecksum_WithSampleData_ShouldReturn1928()
+    {
+        Solver solver = new();
+        List<long> hdd = "0099811188827773336446555566.............."
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        long checksum = solver.CalcChecksum(hdd);
+
+        Assert.Equal(1928, checksum);
+    }
+
+    [Fact]
+    public void CalcChecksum_WithSampleDataFromPart2_ShouldReturn2858()
+    {
+        Solver solver = new();
+        List<long> hdd = "00992111777.44.333....5555.6666.....8888.."
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        long checksum = solver.CalcChecksum(hdd);
+
+        Assert.Equal(2858, checksum);
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe1928()
+    {
+        long checksum = 0;
+        Solver solver = new();
+
+        checksum = long.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(1928, checksum);
+    }
+
+    [Fact]
+    public void DefragContiguous_WithSampleData_ShouldWork()
+    {
+        Solver solver = new();
+        List<long> hdd = "00...111...2...333.44.5555.6666.777.888899"
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        List<long> expected = "00992111777.44.333....5555.6666.....8888.."
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        hdd = solver.DefragContiguous(hdd);
+
+        Assert.Equal(expected, hdd);
+    }
+
+    [Fact]
+    public void DefragContiguous_WithEdgeCase_ShouldWork()
+    {
+        Solver solver = new();
+        List<long> hdd = "00...1112......333.44.5555.6666.777.888899."
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        List<long> expected = "00992111.888844333....5555.6666.777........"
+            .Select(c => c == '.' ? -1 : (long)char.GetNumericValue(c))
+            .ToList();
+        hdd = solver.DefragContiguous(hdd);
+
+        Assert.Equal(expected, hdd);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe2858()
+    {
+        long checksum = 0;
+        Solver solver = new();
+
+        checksum = long.Parse(solver.SolvePart2(data));
+
+        Assert.Equal(2858, checksum);
     }
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+2333133121414131402
 """;
 }
