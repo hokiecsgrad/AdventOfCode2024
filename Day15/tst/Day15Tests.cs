dd1503a5f600be3b7e6357aa2b34e279f2f426be
Ryan Hagan <ryan@ryanhagan.net>
2024-12-14 12:21:49 -0500

    Prep for Day 15

---
+++ Day15/tst/Day15Tests.cs
@@ -0,0 +1,34 @@
+using AdventOfCode.Common;
+using AdventOfCode.Day15;
+using Xunit;
+
+namespace AdventOfCode.Day15.Tests;
+
+public class Day15Tests
+{
+    public Day15Tests()
+    {
+        data = sampleInput.Split(
+            '\n',
+            StringSplitOptions.TrimEntries |
+            StringSplitOptions.RemoveEmptyEntries
+            );
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBeXXX()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBeXXX()
+    {
+        throw new NotImplementedException();
+    }
+
+    private string[] data;
+    private string sampleInput = """
+SAMPLE INPUT HERE
+""";
+}
