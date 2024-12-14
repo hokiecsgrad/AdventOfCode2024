8edbd8eea8b24543df8a3f535ac8cd75c772b0f5
Ryan Hagan <ryan@ryanhagan.net>
2024-12-13 20:29:01 -0500

    Solved Day 13 Part 1 & 2

--- Day13/src/Solver.cs
+++ Day13/src/Solver.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Day13;
@@ -6,11 +8,167 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        long total = 0;
+
+        long[] a = new long[2];
+        long[] b = new long[2];
+        long[] prize = new long[2];
+
+        (double a, double b) buttonPresses;
+        bool aValid;
+        bool bValid;
+
+        foreach (string line in data)
+        {
+            if (line == string.Empty)
+            {
+                buttonPresses = SolveMatrix(a, b, prize);
+
+                aValid = buttonPresses.a >= 0 &&
+                    Math.Abs(Math.Round(buttonPresses.a) - buttonPresses.a) <= 0.000001;
+                bValid = buttonPresses.b >= 0 &&
+                    Math.Abs(Math.Round(buttonPresses.b) - buttonPresses.b) <= 0.000001;
+
+                if (aValid && bValid)
+                    total += ((long)Math.Round(buttonPresses.a) * 3) + (long)Math.Round(buttonPresses.b);
+
+                a = new long[2];
+                b = new long[2];
+                prize = new long[2];
+            }
+            else
+            {
+                (string, long[]) value = ParseInputLine(line);
+                if (value.Item1 == "a") a = value.Item2;
+                if (value.Item1 == "b") b = value.Item2;
+                if (value.Item1 == "prize") prize = value.Item2;
+            }
+        }
+
+        buttonPresses = SolveMatrix(a, b, prize);
+
+        aValid = buttonPresses.a >= 0 &&
+            Math.Abs(Math.Round(buttonPresses.a) - buttonPresses.a) <= 0.000001;
+        bValid = buttonPresses.b >= 0 &&
+            Math.Abs(Math.Round(buttonPresses.b) - buttonPresses.b) <= 0.000001;
+
+        if (aValid && bValid)
+            total += ((long)Math.Round(buttonPresses.a) * 3) + (long)Math.Round(buttonPresses.b);
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        long total = 0;
+
+        long[] a = new long[2];
+        long[] b = new long[2];
+        long[] prize = new long[2];
+
+        (double a, double b) buttonPresses;
+        bool aValid;
+        bool bValid;
+
+        foreach (string line in data)
+        {
+            if (line == string.Empty)
+            {
+                prize[0] += 10_000_000_000_000;
+                prize[1] += 10_000_000_000_000;
+                buttonPresses = SolveMatrix(a, b, prize);
+
+                aValid = buttonPresses.a >= 0 &&
+                    Math.Abs(Math.Round(buttonPresses.a) - buttonPresses.a) <= 0.01;
+                bValid = buttonPresses.b >= 0 &&
+                    Math.Abs(Math.Round(buttonPresses.b) - buttonPresses.b) <= 0.01;
+
+                if (aValid && bValid)
+                    total += ((long)Math.Round(buttonPresses.a) * 3) + (long)Math.Round(buttonPresses.b);
+
+                a = new long[2];
+                b = new long[2];
+                prize = new long[2];
+            }
+            else
+            {
+                (string, long[]) value = ParseInputLine(line);
+                if (value.Item1 == "a") a = value.Item2;
+                if (value.Item1 == "b") b = value.Item2;
+                if (value.Item1 == "prize") prize = value.Item2;
+            }
+        }
+
+        prize[0] += 10_000_000_000_000;
+        prize[1] += 10_000_000_000_000;
+        buttonPresses = SolveMatrix(a, b, prize);
+
+        aValid = buttonPresses.a >= 0 &&
+            Math.Abs(Math.Round(buttonPresses.a) - buttonPresses.a) <= 0.01;
+        bValid = buttonPresses.b >= 0 &&
+            Math.Abs(Math.Round(buttonPresses.b) - buttonPresses.b) <= 0.01;
+
+        if (aValid && bValid)
+            total += ((long)Math.Round(buttonPresses.a) * 3) + (long)Math.Round(buttonPresses.b);
+
+        return total.ToString();
+    }
+
+    public (double, double) SolveMatrix(long[] a, long[] b, long[] prize)
+    {
+        double[,] matrix = new double[2, 3] {
+            { a[0], b[0], prize[0] },
+            { a[1], b[1], prize[1] }
+        };
+        // get 1 in matrix[0,0]
+        matrix[0, 1] = matrix[0, 1] / matrix[0, 0];
+        matrix[0, 2] = matrix[0, 2] / matrix[0, 0];
+        matrix[0, 0] = matrix[0, 0] / matrix[0, 0];
+        // get 0 in matrix[1,0]
+        matrix[1, 1] = -matrix[1, 0] * matrix[0, 1] + matrix[1, 1];
+        matrix[1, 2] = -matrix[1, 0] * matrix[0, 2] + matrix[1, 2];
+        matrix[1, 0] = -matrix[1, 0] * matrix[0, 0] + matrix[1, 0];
+        // get 1 in matrix[1,1]
+        matrix[1, 0] = matrix[1, 0] / matrix[1, 1];
+        matrix[1, 2] = matrix[1, 2] / matrix[1, 1];
+        matrix[1, 1] = matrix[1, 1] / matrix[1, 1];
+        // get 0 in matrix[0,1] 
+        matrix[0, 0] = -matrix[0, 1] * matrix[1, 0] + matrix[0, 0];
+        matrix[0, 2] = -matrix[0, 1] * matrix[1, 2] + matrix[0, 2];
+        matrix[0, 1] = -matrix[0, 1] * matrix[1, 1] + matrix[0, 1];
+
+        return (matrix[0, 2], matrix[1, 2]);
+    }
+
+    public (string, long[]) ParseInputLine(string line)
+    {
+        string value = string.Empty;
+        long[] temp = new long[2];
+
+        Regex xpat = new Regex(@"X\+([\d]+),");
+        Regex ypat = new Regex(@"Y\+([\d]+)$");
+        Regex xpatPrize = new Regex(@"X=([\d]+),");
+        Regex ypatPrize = new Regex(@"Y=([\d]+)$");
+
+        if (line.Substring(0, 8) == "Button A")
+        {
+            value = "a";
+            temp[0] = long.Parse(xpat.Match(line).Groups[1].Value);
+            temp[1] = long.Parse(ypat.Match(line).Groups[1].Value);
+        }
+        else if (line.Substring(0, 8) == "Button B")
+        {
+            value = "b";
+            temp[0] = long.Parse(xpat.Match(line).Groups[1].Value);
+            temp[1] = long.Parse(ypat.Match(line).Groups[1].Value);
+        }
+        else if (line.Substring(0, 5) == "Prize")
+        {
+            value = "prize";
+            temp[0] = long.Parse(xpatPrize.Match(line).Groups[1].Value);
+            temp[1] = long.Parse(ypatPrize.Match(line).Groups[1].Value);
+        }
+
+        return (value, temp);
     }
 }
--- Day13/tst/Day13Tests.cs
+++ Day13/tst/Day13Tests.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 using AdventOfCode.Day13;
 using Xunit;
@@ -10,25 +11,86 @@ public class Day13Tests
     {
         data = sampleInput.Split(
             '\n',
-            StringSplitOptions.TrimEntries |
-            StringSplitOptions.RemoveEmptyEntries
+            StringSplitOptions.TrimEntries
             );
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void MatrixSolution_WithSampleData_ShouldBe280()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        long[] a = new long[2] { 94, 34 };
+        long[] b = new long[2] { 22, 67 };
+        long[] prize = new long[2] { 8400, 5400 };
+        (double a, double b) buttonPresses = solver.SolveMatrix(a, b, prize);
+
+        double total = buttonPresses.a * 3 + buttonPresses.b;
+
+        Assert.Equal(80.0, buttonPresses.a, 1);
+        Assert.Equal(40.0, buttonPresses.b, 1);
+        Assert.Equal(280.0, total, 1);
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void MatrixSolution_WithBadData_ShouldNotWork()
     {
-        throw new NotImplementedException();
+        double total = 0.0;
+        Solver solver = new();
+
+        long[] a = new long[2] { 26, 66 };
+        long[] b = new long[2] { 67, 21 };
+        long[] prize = new long[2] { 12748, 12176 };
+
+        (double a, double b) buttonPresses = solver.SolveMatrix(a, b, prize);
+
+        bool hasSolution = false;
+        if (Math.Abs(buttonPresses.a % 1) <= (Double.Epsilon * 100) &&
+            Math.Abs(buttonPresses.b % 1) <= (Double.Epsilon * 100))
+        {
+            hasSolution = true;
+            total = buttonPresses.a * 3 + buttonPresses.b;
+        }
+
+        Assert.False(hasSolution);
+        Assert.Equal(0.0, total, 1);
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe480()
+    {
+        Solver solver = new();
+
+        double total = double.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(480, total, 1);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe875318608908()
+    {
+        Solver solver = new();
+
+        double total = double.Parse(solver.SolvePart2(data));
+
+        Assert.Equal(875318608908, total, 1);
     }
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
 """;
 }
