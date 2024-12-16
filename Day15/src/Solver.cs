fd48a99586dfc5b08dd5ce1867f68361b37601b2
Ryan Hagan <ryan@ryanhagan.net>
2024-12-16 08:29:47 -0500

    Solved Day 15 part 1 & 2

--- Day15/src/Solver.cs
+++ Day15/src/Solver.cs
@@ -6,11 +6,352 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        char[,] grid = ParseGrid(data);
+        List<char> instructions = ParseInstructions(data);
+        Point robot = FindCharInGrid(grid, '@');
+        Point newPos = new Point(robot.Row, robot.Col);
+
+        foreach (char dir in instructions)
+            newPos = MoveRobot(ref grid, newPos, dir);
+
+        long gps = CalculateGps(grid);
+
+        return gps.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        char[,] smallGrid = ParseGrid(data);
+        char[,] grid = ScaleUpGrid(smallGrid);
+        List<char> instructions = ParseInstructions(data);
+        Point robot = FindCharInGrid(grid, '@');
+        Point newPos = new Point(robot.Row, robot.Col);
+
+        foreach (char dir in instructions)
+            newPos = MoveRobot(ref grid, newPos, dir);
+
+        long gps = CalculateLargeGps(grid);
+
+        return gps.ToString();
+    }
+
+    public Point MoveRobot(ref char[,] grid, Point robot, char dir)
+    {
+        Point moveDir = dir switch
+        {
+            '<' => new Point(0, -1),
+            '^' => new Point(-1, 0),
+            '>' => new Point(0, 1),
+            'v' => new Point(1, 0),
+            _ => new Point(-1, -1)
+        };
+
+        Point newPos = new Point(robot.Row + moveDir.Row, robot.Col + moveDir.Col);
+
+        if (!PointIsInBounds(grid, newPos) || IsWall(grid, newPos))
+            return robot;
+
+        if (IsSmallBox(grid, newPos) || (IsBigBox(grid, newPos) && (dir == '<' || dir == '>')))
+        {
+            Point testPoint = new Point(newPos.Row, newPos.Col);
+            while (PointIsInBounds(grid, testPoint) && IsBox(grid, testPoint))
+                testPoint = new Point(testPoint.Row + moveDir.Row, testPoint.Col + moveDir.Col);
+
+            if (IsWall(grid, testPoint))
+            {
+                return robot;
+            }
+            else if (IsEmpty(grid, testPoint))
+            {
+                Point revDir = new Point(moveDir.Row, moveDir.Col);
+                revDir.Row = moveDir.Row * -1;
+                revDir.Col = moveDir.Col * -1;
+                while (!IsRobot(grid, testPoint))
+                {
+                    grid[testPoint.Row, testPoint.Col] =
+                        grid[testPoint.Row + revDir.Row, testPoint.Col + revDir.Col];
+                    testPoint = new Point(testPoint.Row + revDir.Row, testPoint.Col + revDir.Col);
+                }
+                grid[testPoint.Row, testPoint.Col] = '.';
+                newPos = new Point(testPoint.Row + moveDir.Row, testPoint.Col + moveDir.Col);
+            }
+        }
+        else if (IsBigBox(grid, newPos))
+        {
+            List<Point> box = new();
+            if (grid[newPos.Row, newPos.Col] == '[')
+            {
+                box.Add(new Point(newPos.Row, newPos.Col));
+                box.Add(new Point(newPos.Row, newPos.Col + 1));
+            }
+            else
+            {
+                box.Add(new Point(newPos.Row, newPos.Col - 1));
+                box.Add(new Point(newPos.Row, newPos.Col));
+            }
+            if (CanMoveBigBoxUp(grid, box, dir == '^' ? -1 : 1))
+            {
+                MoveBigBoxUp(ref grid, box, dir == '^' ? -1 : 1);
+                grid[newPos.Row, newPos.Col] = '@';
+                grid[robot.Row, robot.Col] = '.';
+            }
+            else
+                return robot;
+        }
+        else
+        {
+            grid[newPos.Row, newPos.Col] = '@';
+            grid[robot.Row, robot.Col] = '.';
+        }
+
+        return newPos;
+    }
+
+    public bool CanMoveBigBoxUp(char[,] grid, List<Point> boxCoords, int dir)
+    {
+        if (IsWall(grid, new Point(boxCoords[0].Row + dir, boxCoords[0].Col)) || IsWall(grid, new Point(boxCoords[1].Row + dir, boxCoords[1].Col)))
+            return false;
+        if (IsEmpty(grid, new Point(boxCoords[0].Row + dir, boxCoords[0].Col)) && IsEmpty(grid, new Point(boxCoords[1].Row + dir, boxCoords[1].Col)))
+            return true;
+
+        bool canMoveUp = true;
+
+        if (grid[boxCoords[0].Row + dir, boxCoords[0].Col] == '[')
+            canMoveUp = canMoveUp && CanMoveBigBoxUp(grid, new List<Point> { new Point(boxCoords[0].Row + dir, boxCoords[0].Col), new Point(boxCoords[1].Row + dir, boxCoords[1].Col) }, dir);
+        else if (grid[boxCoords[0].Row + dir, boxCoords[0].Col] == ']' && IsEmpty(grid, new Point(boxCoords[1].Row + dir, boxCoords[1].Col)))
+            canMoveUp = canMoveUp && CanMoveBigBoxUp(grid, new List<Point> { new Point(boxCoords[0].Row + dir, boxCoords[0].Col - 1), new Point(boxCoords[0].Row + dir, boxCoords[0].Col) }, dir);
+        else if (IsEmpty(grid, new Point(boxCoords[0].Row + dir, boxCoords[0].Col)) && grid[boxCoords[1].Row + dir, boxCoords[1].Col] == '[')
+            canMoveUp = canMoveUp && CanMoveBigBoxUp(grid, new List<Point> { new Point(boxCoords[1].Row + dir, boxCoords[1].Col), new Point(boxCoords[1].Row + dir, boxCoords[1].Col + 1) }, dir);
+        else if (grid[boxCoords[0].Row + dir, boxCoords[0].Col] == ']' && grid[boxCoords[1].Row + dir, boxCoords[1].Col] == '[')
+        {
+            canMoveUp = canMoveUp && CanMoveBigBoxUp(grid, new List<Point> { new Point(boxCoords[0].Row + dir, boxCoords[0].Col - 1), new Point(boxCoords[0].Row + dir, boxCoords[0].Col) }, dir);
+            canMoveUp = canMoveUp && CanMoveBigBoxUp(grid, new List<Point> { new Point(boxCoords[1].Row + dir, boxCoords[1].Col), new Point(boxCoords[1].Row + dir, boxCoords[1].Col + 1) }, dir);
+        }
+
+        return canMoveUp;
+    }
+
+    public void MoveBigBoxUp(ref char[,] grid, List<Point> boxCoords, int dir)
+    {
+        if (IsWall(grid, new Point(boxCoords[0].Row + dir, boxCoords[0].Col)) || IsWall(grid, new Point(boxCoords[1].Row + dir, boxCoords[1].Col)))
+            return;
+
+        if (grid[boxCoords[0].Row + dir, boxCoords[0].Col] == '[')
+            MoveBigBoxUp(ref grid, new List<Point> { new Point(boxCoords[0].Row + dir, boxCoords[0].Col), new Point(boxCoords[1].Row + dir, boxCoords[1].Col) }, dir);
+        else if (grid[boxCoords[0].Row + dir, boxCoords[0].Col] == ']' && IsEmpty(grid, new Point(boxCoords[1].Row + dir, boxCoords[1].Col)))
+            MoveBigBoxUp(ref grid, new List<Point> { new Point(boxCoords[0].Row + dir, boxCoords[0].Col - 1), new Point(boxCoords[0].Row + dir, boxCoords[0].Col) }, dir);
+        else if (IsEmpty(grid, new Point(boxCoords[0].Row + dir, boxCoords[0].Col)) && grid[boxCoords[1].Row + dir, boxCoords[1].Col] == '[')
+            MoveBigBoxUp(ref grid, new List<Point> { new Point(boxCoords[1].Row + dir, boxCoords[1].Col), new Point(boxCoords[1].Row + dir, boxCoords[1].Col + 1) }, dir);
+        else if (grid[boxCoords[0].Row + dir, boxCoords[0].Col] == ']' && grid[boxCoords[1].Row + dir, boxCoords[1].Col] == '[')
+        {
+            MoveBigBoxUp(ref grid, new List<Point> { new Point(boxCoords[0].Row + dir, boxCoords[0].Col - 1), new Point(boxCoords[0].Row + dir, boxCoords[0].Col) }, dir);
+            MoveBigBoxUp(ref grid, new List<Point> { new Point(boxCoords[1].Row + dir, boxCoords[1].Col), new Point(boxCoords[1].Row + dir, boxCoords[1].Col + 1) }, dir);
+        }
+
+        if (IsEmpty(grid, new Point(boxCoords[0].Row + dir, boxCoords[0].Col)) && IsEmpty(grid, new Point(boxCoords[1].Row + dir, boxCoords[1].Col)))
+        {
+            grid[boxCoords[0].Row + dir, boxCoords[0].Col] = '[';
+            grid[boxCoords[1].Row + dir, boxCoords[1].Col] = ']';
+            grid[boxCoords[0].Row, boxCoords[0].Col] = '.';
+            grid[boxCoords[1].Row, boxCoords[1].Col] = '.';
+            return;
+        }
+    }
+
+    public long CalculateGps(char[,] grid)
+    {
+        long total = 0;
+        for (int row = 0; row < grid.GetLength(0); row++)
+            for (int col = 0; col < grid.GetLength(1); col++)
+                if (grid[row, col] == 'O')
+                    total += (100 * row) + col;
+        return total;
+    }
+
+    public long CalculateLargeGps(char[,] grid)
+    {
+        long total = 0;
+        int midRow = grid.GetLength(0) / 2;
+        int midCol = grid.GetLength(1) / 2;
+        for (int row = 0; row < grid.GetLength(0); row++)
+            for (int col = 0; col < grid.GetLength(1); col++)
+                if (grid[row, col] == '[')
+                    total += (100 * row) + col;
+        return total;
+    }
+
+    public char[,] ScaleUpGrid(char[,] grid)
+    {
+        char[,] newGrid = new char[grid.GetLength(0), grid.GetLength(1) * 2];
+
+        for (int row = 0; row < grid.GetLength(0); row++)
+        {
+            for (int col = 0; col < grid.GetLength(1); col++)
+            {
+                if (grid[row, col] == '#' || grid[row, col] == '.')
+                {
+                    newGrid[row, col * 2] = grid[row, col];
+                    newGrid[row, col * 2 + 1] = grid[row, col];
+                }
+                else if (grid[row, col] == 'O')
+                {
+                    newGrid[row, col * 2] = '[';
+                    newGrid[row, col * 2 + 1] = ']';
+                }
+                else if (grid[row, col] == '@')
+                {
+                    newGrid[row, col * 2] = '@';
+                    newGrid[row, col * 2 + 1] = '.';
+                }
+            }
+        }
+
+        return newGrid;
+    }
+
+    public char[,] ParseGrid(string[] data)
+    {
+        int maxRow = 0;
+        while (data[maxRow] != string.Empty) maxRow++;
+        char[,] grid = new char[maxRow, data[0].Length];
+
+        int row = 0;
+        while (row < maxRow)
+        {
+            for (int col = 0; col < data[row].Length; col++)
+                grid[row, col] = data[row][col];
+            row++;
+        }
+
+        return grid;
+    }
+
+    public List<char> ParseInstructions(string[] data)
+    {
+        List<char> instructions = new();
+        int row = 0;
+        while (data[row] != string.Empty) row++;
+        row++;
+
+        while (row < data.Length)
+        {
+            for (int col = 0; col < data[row].Length; col++)
+                instructions.Add(data[row][col]);
+            row++;
+        }
+
+        return instructions;
+    }
+
+    public Point FindCharInGrid(char[,] grid, char target)
+    {
+        for (int row = 0; row < grid.GetLength(0); row++)
+            for (int col = 0; col < grid.GetLength(1); col++)
+                if (grid[row, col] == target)
+                    return new Point(row, col);
+        return new Point(-1, -1);
+    }
+
+    public bool IsWall(char[,] grid, Point pos)
+    {
+        return grid[pos.Row, pos.Col] == '#';
+    }
+
+    public bool IsBox(char[,] grid, Point pos)
+    {
+        return grid[pos.Row, pos.Col] == 'O' ||
+            grid[pos.Row, pos.Col] == '[' ||
+            grid[pos.Row, pos.Col] == ']';
+    }
+
+    public bool IsSmallBox(char[,] grid, Point pos)
+    {
+        return grid[pos.Row, pos.Col] == 'O';
+    }
+
+    public bool IsBigBox(char[,] grid, Point pos)
+    {
+        return grid[pos.Row, pos.Col] == '[' ||
+            grid[pos.Row, pos.Col] == ']';
+    }
+
+    public bool IsRobot(char[,] grid, Point pos)
+    {
+        return grid[pos.Row, pos.Col] == '@';
+    }
+
+    public bool IsEmpty(char[,] grid, Point pos)
+    {
+        return grid[pos.Row, pos.Col] == '.';
+    }
+
+    public bool PointIsInBounds(char[,] grid, Point pos)
+    {
+        return pos.Row >= 0 && pos.Row < grid.GetLength(0) &&
+                pos.Col >= 0 && pos.Col < grid.GetLength(1);
+    }
+}
+
+public class Point : IComparable<Point>
+{
+    public int Row { get; set; }
+    public int Col { get; set; }
+
+    public Point(int row, int col)
+    {
+        Row = row;
+        Col = col;
+    }
+
+    public int CompareTo(object obj)
+    {
+        if (obj == null) return 1;
+
+        if (obj is Point otherPoint)
+            return CompareTo(otherPoint);
+        else
+            throw new ArgumentException("Object is not a Point");
+    }
+
+    public int CompareTo(Point other)
+    {
+        if (other == null) return 1;
+
+        int rowComparison = this.Row.CompareTo(other.Row);
+        if (rowComparison != 0)
+        {
+            return rowComparison;
+        }
+        else
+        {
+            return this.Col.CompareTo(other.Col);
+        }
+    }
+
+    public static bool operator ==(Point p1, Point p2)
+    {
+        if (ReferenceEquals(p1, p2)) return true;
+        if (p1 is null || p2 is null) return false;
+
+        return p1.Row == p2.Row && p1.Col == p2.Col;
+    }
+
+    public static bool operator !=(Point p1, Point p2)
+    {
+        return !(p1 == p2);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Point point)
+        {
+            return this == point;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Row, Col);
     }
 }
--- Day15/tst/Day15Tests.cs
+++ Day15/tst/Day15Tests.cs
@@ -10,25 +10,374 @@ public class Day15Tests
     {
         data = sampleInput.Split(
             '\n',
-            StringSplitOptions.TrimEntries |
-            StringSplitOptions.RemoveEmptyEntries
+            StringSplitOptions.TrimEntries
+            );
+        dataSimp = simpleInput.Split(
+            '\n',
+            StringSplitOptions.TrimEntries
             );
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void ParseData_WithSimpleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        char[,] grid = solver.ParseGrid(dataSimp);
+        List<char> instructions = solver.ParseInstructions(dataSimp);
+
+        Assert.Equal(8, grid.GetLength(0));
+        Assert.Equal(8, grid.GetLength(1));
+        Assert.Equal(15, instructions.Count());
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void FindChar_WithSimpleData_ShouldBe22()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        char[,] grid = solver.ParseGrid(dataSimp);
+        Point robot = solver.FindCharInGrid(grid, '@');
+
+        Assert.Equal(new Point(2, 2), robot);
+    }
+
+    [Fact]
+    public void MoveRobot_WithNoBlockers_ShouldWork()
+    {
+        Solver solver = new();
+        char[,] grid = new char[2, 2] { { '.', '.' }, { '.', '@' } };
+        List<char> instructions = new List<char> { '<', '^', '>', 'v' };
+        Point newPos = new Point(-1, -1);
+
+        newPos = solver.MoveRobot(ref grid, new Point(1, 1), instructions[0]);
+        Assert.Equal('@', grid[newPos.Row, newPos.Col]);
+        Assert.Equal('.', grid[1, 1]);
+        newPos = solver.MoveRobot(ref grid, new Point(1, 0), instructions[1]);
+        Assert.Equal('@', grid[newPos.Row, newPos.Col]);
+        Assert.Equal('.', grid[1, 0]);
+        newPos = solver.MoveRobot(ref grid, new Point(0, 0), instructions[2]);
+        Assert.Equal('@', grid[newPos.Row, newPos.Col]);
+        Assert.Equal('.', grid[0, 0]);
+        newPos = solver.MoveRobot(ref grid, new Point(0, 1), instructions[3]);
+        Assert.Equal('@', grid[newPos.Row, newPos.Col]);
+        Assert.Equal('.', grid[0, 1]);
+        newPos = solver.MoveRobot(ref grid, new Point(1, 1), '>');
+        Assert.Equal('@', grid[newPos.Row, newPos.Col]);
+        Assert.Equal('.', grid[0, 1]);
+        newPos = solver.MoveRobot(ref grid, new Point(1, 1), 'v');
+        Assert.Equal('@', grid[newPos.Row, newPos.Col]);
+        Assert.Equal('.', grid[0, 1]);
+    }
+
+    [Fact]
+    public void MoveRobot_WithBlockers_ShouldWork()
+    {
+        Solver solver = new();
+        char[,] grid = solver.ParseGrid(dataSimp);
+        List<char> instructions = new List<char> { '<', '^', '^', '^', '^', '^' };
+        Point newPos = new Point(2, 2);
+
+        foreach (char dir in instructions)
+            newPos = solver.MoveRobot(ref grid, newPos, dir);
+
+        Assert.Equal(new Point(1, 2), newPos);
+    }
+
+    [Fact]
+    public void MoveRobot_WithOneSmallBoxNoBlocker_ShouldWork()
+    {
+        Solver solver = new();
+        char[,] grid = solver.ParseGrid(data);
+        List<char> instructions = new List<char> { '<' };
+        Point newPos = new Point(4, 4);
+
+        foreach (char dir in instructions)
+            newPos = solver.MoveRobot(ref grid, newPos, dir);
+
+        Assert.Equal(new Point(4, 3), newPos);
+        Assert.Equal('O', grid[4, 2]);
+        Assert.Equal('.', grid[4, 1]);
+        Assert.Equal('.', grid[4, 4]);
+    }
+
+    [Fact]
+    public void MoveRobot_WithOneBigBoxNoBlocker_ShouldWork()
+    {
+        Solver solver = new();
+        Point newPos;
+        char[,] smallGrid = solver.ParseGrid(data);
+        char[,] grid = solver.ScaleUpGrid(smallGrid);
+
+        Point robot = solver.FindCharInGrid(grid, '@');
+
+        newPos = solver.MoveRobot(ref grid, robot, '<');
+
+        Assert.Equal('[', grid[4, 5]);
+        Assert.Equal(']', grid[4, 6]);
+        Assert.Equal('@', grid[4, 7]);
+    }
+
+    [Fact]
+    public void MoveRobotLeft_WithOneBigBoxOneBlocker_ShouldMoveToEdge()
+    {
+        Solver solver = new();
+        Point newPos;
+        char[,] smallGrid = solver.ParseGrid(data);
+        char[,] grid = solver.ScaleUpGrid(smallGrid);
+
+        Point robot = solver.FindCharInGrid(grid, '@');
+        newPos = new Point(robot.Row, robot.Col);
+
+        newPos = solver.MoveRobot(ref grid, newPos, '<');
+        newPos = solver.MoveRobot(ref grid, newPos, '<');
+        newPos = solver.MoveRobot(ref grid, newPos, '<');
+        newPos = solver.MoveRobot(ref grid, newPos, '<');
+        newPos = solver.MoveRobot(ref grid, newPos, '<');
+        newPos = solver.MoveRobot(ref grid, newPos, '<');
+
+        Assert.Equal('[', grid[4, 2]);
+        Assert.Equal(']', grid[4, 3]);
+        Assert.Equal('@', grid[4, 4]);
+        Assert.Equal('.', grid[4, 5]);
+        Assert.Equal('.', grid[4, 6]);
+        Assert.Equal('.', grid[4, 7]);
+        Assert.Equal('.', grid[4, 8]);
+    }
+
+    [Fact]
+    public void MoveRobotUp_WithOneBigBoxNoBlocker_ShouldMoveUp()
+    {
+        Solver solver = new();
+        char[,] grid = new char[4, 3] {
+            { '#', '#', '#' },
+            { '.', '.', '.' },
+            { '.', '[', ']' },
+            { '.', '@', '.' } };
+
+        bool canMove = solver.CanMoveBigBoxUp(grid, new List<Point> { new Point(2, 1), new Point(2, 2) }, -1);
+
+        Assert.True(canMove);
+    }
+
+    [Fact]
+    public void MoveRobotUp_WithLotsBigBoxNoBlocker_ShouldMoveUp()
+    {
+        Solver solver = new();
+        char[,] grid = new char[6, 3] {
+            { '#', '#', '#' },
+            { '.', '.', '.' },
+            { '.', '[', ']' },
+            { '[', ']', '.' },
+            { '[', ']', '.' },
+            { '.', '@', '.' },
+            };
+
+        bool canMove = solver.CanMoveBigBoxUp(grid, new List<Point> { new Point(4, 0), new Point(4, 1) }, -1);
+        if (canMove)
+        {
+            solver.MoveBigBoxUp(ref grid, new List<Point> { new Point(4, 0), new Point(4, 1) }, -1);
+            grid[4, 1] = '@';
+            grid[5, 1] = '.';
+        }
+
+        char[,] expected = new char[6, 3] {
+            { '#', '#', '#' },
+            { '.', '[', ']' },
+            { '[', ']', '.' },
+            { '[', ']', '.' },
+            { '.', '@', '.' },
+            { '.', '.', '.' },
+            };
+
+        Assert.True(canMove);
+        Assert.Equal(expected, grid);
+    }
+
+    [Fact]
+    public void MoveRobotDown_WithLotsBigBoxNoBlocker_ShouldMoveUp()
+    {
+        Solver solver = new();
+        char[,] grid = new char[6, 3] {
+            { '#', '#', '#' },
+            { '.', '@', '.' },
+            { '.', '[', ']' },
+            { '[', ']', '.' },
+            { '[', ']', '.' },
+            { '.', '.', '.' },
+            };
+
+        bool canMove = solver.CanMoveBigBoxUp(grid, new List<Point> { new Point(2, 1), new Point(2, 2) }, 1);
+        if (canMove)
+        {
+            solver.MoveBigBoxUp(ref grid, new List<Point> { new Point(2, 1), new Point(2, 2) }, 1);
+            grid[2, 1] = '@';
+            grid[1, 1] = '.';
+        }
+
+        char[,] expected = new char[6, 3] {
+            { '#', '#', '#' },
+            { '.', '.', '.' },
+            { '.', '@', '.' },
+            { '.', '[', ']' },
+            { '[', ']', '.' },
+            { '[', ']', '.' },
+            };
+
+        Assert.True(canMove);
+        Assert.Equal(expected, grid);
+    }
+
+    [Fact]
+    public void MoveRobot_WithOneBoxAndBlockers_ShouldNotMove()
+    {
+        Solver solver = new();
+        char[,] grid = solver.ParseGrid(data);
+        List<char> instructions = new List<char> { '<', '<', '<' };
+        Point newPos = new Point(4, 4);
+
+        foreach (char dir in instructions)
+            newPos = solver.MoveRobot(ref grid, newPos, dir);
+
+        Assert.Equal(new Point(4, 2), newPos);
+        Assert.Equal('O', grid[4, 1]);
+        Assert.Equal('.', grid[4, 3]);
+        Assert.Equal('.', grid[4, 4]);
+    }
+
+    [Fact]
+    public void Part1_WithSimpleData_ShouldBe2028()
+    {
+        Solver solver = new();
+
+        long gps = long.Parse(solver.SolvePart1(dataSimp));
+
+        Assert.Equal(2028, gps);
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe10092()
+    {
+        Solver solver = new();
+
+        long gps = long.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(10092, gps);
+    }
+
+    [Fact]
+    public void ScaleUp_WithSampleData_ShouldWork()
+    {
+        Solver solver = new();
+        char[,] smallGrid = solver.ParseGrid(data);
+        char[,] grid = solver.ScaleUpGrid(smallGrid);
+
+        Point robot = solver.FindCharInGrid(grid, '@');
+
+        string[] expectedStr = sampleInputScaled.Split(
+            '\n',
+            StringSplitOptions.TrimEntries
+            );
+        char[,] expected = solver.ParseGrid(expectedStr);
+
+        Assert.Equal(expected, grid);
+        Assert.Equal(10, grid.GetLength(0));
+        Assert.Equal(20, grid.GetLength(1));
+        Assert.Equal(new Point(4, 8), robot);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldMatchExpectedGrid()
+    {
+        Solver solver = new();
+        char[,] smallGrid = solver.ParseGrid(data);
+        char[,] grid = solver.ScaleUpGrid(smallGrid);
+        List<char> instructions = solver.ParseInstructions(data);
+
+        Point robot = solver.FindCharInGrid(grid, '@');
+        Point newPos = new Point(robot.Row, robot.Col);
+
+        foreach (char dir in instructions)
+            newPos = solver.MoveRobot(ref grid, newPos, dir);
+
+        string[] expectedStr = sampleInputScaledExpected.Split(
+            '\n',
+            StringSplitOptions.TrimEntries
+            );
+        char[,] expected = solver.ParseGrid(expectedStr);
+
+        Assert.Equal(expected, grid);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe9021()
+    {
+        Solver solver = new();
+
+        long gps = long.Parse(solver.SolvePart2(data));
+
+        Assert.Equal(9021, gps);
     }
 
     private string[] data;
+    private string[] dataSimp;
     private string sampleInput = """
-SAMPLE INPUT HERE
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+""";
+    private string simpleInput = """
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+""";
+    private string sampleInputScaled = """
+####################
+##....[]....[]..[]##
+##............[]..##
+##..[][]....[]..[]##
+##....[]@.....[]..##
+##[]##....[]......##
+##[]....[]....[]..##
+##..[][]..[]..[][]##
+##........[]......##
+####################
+
+""";
+    private string sampleInputScaledExpected = """
+####################
+##[].......[].[][]##
+##[]...........[].##
+##[]........[][][]##
+##[]......[]....[]##
+##..##......[]....##
+##..[]............##
+##..@......[].[][]##
+##......[][]..[]..##
+####################
+
 """;
 }
