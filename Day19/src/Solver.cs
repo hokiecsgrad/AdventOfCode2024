cf9ae7935ba22450865045eb77c7295e99cb4a1f
Ryan Hagan <ryan@ryanhagan.net>
2024-12-19 22:03:11 -0500

    Solved Day 19 parts 1 & 2

--- Day19/src/Solver.cs
+++ Day19/src/Solver.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Day19;
@@ -6,11 +7,87 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        int total = 0;
+
+        List<string> patterns = ParsePatterns(data);
+        List<string> designs = ParseDesigns(data);
+
+        total = CountMatchingDesigns(designs, patterns);
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
-        return String.Empty;
+        long total = 0;
+
+        List<string> patterns = ParsePatterns(data);
+        List<string> designs = ParseDesigns(data);
+
+        total = CountAllDesignCombos(designs, patterns);
+
+        return total.ToString();
     }
+
+    public int CountMatchingDesigns(List<string> designs, List<string> patterns)
+    {
+        int total = 0;
+
+        List<string> sortedPats = patterns.OrderByDescending(x => x.Length).ToList();
+        Dictionary<string, long> cache = new();
+
+        foreach (string design in designs)
+            if (CountNumDesigns(design, sortedPats, string.Empty, cache) > 0)
+                total++;
+
+        return total;
+    }
+
+    public long CountAllDesignCombos(List<string> designs, List<string> patterns)
+    {
+        long total = 0;
+
+        List<string> sortedPats = patterns.OrderBy(x => x.Length).ToList();
+        Dictionary<string, long> cache = new();
+
+        foreach (string design in designs)
+            total += CountNumDesigns(design, sortedPats, string.Empty, cache);
+
+        return total;
+    }
+
+    public long CountNumDesigns(string design, List<string> patterns, string curr, Dictionary<string, long> cache)
+    {
+        if (curr == design) return 1;
+        string remaining = design.Substring(curr.Length);
+        if (cache.ContainsKey(remaining)) return cache[remaining];
+        if (curr.Length > design.Length) return 0;
+
+        long totalCombos = 0;
+
+        int testIndex = curr.Length;
+        for (int i = 0; i < patterns.Count; i++)
+        {
+            int patLen = patterns[i].Length;
+            if (curr.Length + patLen <= design.Length &&
+                design.Substring(testIndex, patLen) == patterns[i])
+            {
+                totalCombos += CountNumDesigns(design, patterns, curr + patterns[i], cache);
+                if (cache.ContainsKey(remaining))
+                    cache[remaining] = totalCombos;
+                else
+                    cache.Add(remaining, totalCombos);
+            }
+        }
+
+        return totalCombos;
+    }
+
+    public List<string> ParsePatterns(string[] data)
+        => data[0]
+            .Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+    public List<string> ParseDesigns(string[] data)
+        => data.Skip(2).ToList();
 }
--- Day19/tst/Day19Tests.cs
+++ Day19/tst/Day19Tests.cs
@@ -10,25 +10,61 @@ public class Day19Tests
     {
         data = sampleInput.Split(
             '\n',
-            StringSplitOptions.TrimEntries |
-            StringSplitOptions.RemoveEmptyEntries
+            StringSplitOptions.TrimEntries
             );
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void ParsePatterns_WithSampleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        List<string> patterns = solver.ParsePatterns(data);
+        List<string> expected = new List<string> { "r", "wr", "b", "g", "bwu", "rb", "gb", "br" };
+        Assert.Equal(expected, patterns);
     }
 
     [Fact]
-    public void Part2_WithSampleData_ShouldBeXXX()
+    public void ParseDesigns_WithSampleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+        List<string> designs = solver.ParseDesigns(data);
+
+        Assert.Equal(8, designs.Count);
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe6()
+    {
+        Solver solver = new();
+        int total = 0;
+
+        total = int.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(6, total);
+    }
+
+    [Fact]
+    public void Part2_WithSampleData_ShouldBe16()
+    {
+        Solver solver = new();
+        int total = 0;
+
+        total = int.Parse(solver.SolvePart2(data));
+
+        Assert.Equal(16, total);
     }
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb
 """;
 }
