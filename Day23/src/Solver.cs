beae5c698006a524b06aa26555bc7e2df46b6d8e
Ryan Hagan <ryan@ryanhagan.net>
2024-12-24 08:09:06 -0500

    Solved Day 23 part 1

--- Day23/src/Solver.cs
+++ Day23/src/Solver.cs
@@ -6,11 +6,80 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        (List<string> computers, Dictionary<string, HashSet<string>> pairs) parsed =
+            ParseData(data);
+        List<string> comps = parsed.computers;
+        Dictionary<string, HashSet<string>> pairs = parsed.pairs;
+
+        List<List<string>> trips = GetTrips(comps, pairs);
+
+        long total = trips.Where(c => c[0].StartsWith("t") || c[1].StartsWith("t") || c[2].StartsWith("t")).Count();
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
         return String.Empty;
+    }
+
+    public List<List<string>> GetTrips(List<string> comps, Dictionary<string, HashSet<string>> pairs)
+    {
+        List<List<string>> trips = new();
+
+        foreach (string comp in comps)
+        {
+            List<List<string>> allTrips = FindConnections(comp, new List<string>() { comp }, pairs);
+            foreach (List<string> trip in allTrips)
+            {
+                List<string> orderedTrip = trip.OrderBy(x => x).ToList();
+                if (orderedTrip.Count == 3 && !trips.Any(t => t.SequenceEqual(orderedTrip)))
+                    trips.Add(orderedTrip);
+            }
+        }
+
+        return trips;
+    }
+
+    public List<List<string>> FindConnections(
+        string comp,
+        List<string> curr,
+        Dictionary<string, HashSet<string>> pairs)
+    {
+        if (curr.Count == 3 && pairs[comp].Contains(curr[0])) return new List<List<string>>() { curr };
+        if (curr.Count == 3) return new List<List<string>>();
+
+        List<string> connections = pairs[comp].ToList();
+        List<List<string>> allTrips = new();
+        foreach (string connection in connections)
+        {
+            if (!curr.Contains(connection))
+                allTrips.AddRange(FindConnections(connection, new List<string>(curr) { connection }, pairs));
+        }
+
+        return allTrips;
+    }
+
+    public (List<string>, Dictionary<string, HashSet<string>>) ParseData(string[] data)
+    {
+        Dictionary<string, HashSet<string>> pairs = new();
+        HashSet<string> computers = new();
+        foreach (string pair in data)
+        {
+            string first = pair.Substring(0, 2);
+            string second = pair.Substring(3, 2);
+            if (!pairs.ContainsKey(first))
+                pairs.Add(first, new());
+            pairs[first].Add(second);
+            if (!pairs.ContainsKey(second))
+                pairs.Add(second, new());
+            pairs[second].Add(first);
+            computers.Add(first);
+            computers.Add(second);
+        }
+
+        List<string> sorted = computers.OrderBy(x => x).ToList();
+
+        return (sorted, pairs);
     }
 }
--- Day23/tst/Day23Tests.cs
+++ Day23/tst/Day23Tests.cs
@@ -16,9 +16,42 @@ public class Day23Tests
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void CreateSetOfComputers_WithSampleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+
+        (List<string> computers, Dictionary<string, HashSet<string>> pairs) parsed =
+            solver.ParseData(data);
+        List<string> comps = parsed.computers;
+        Dictionary<string, HashSet<string>> pairs = parsed.pairs;
+
+        Assert.Equal(16, comps.Count);
+        Assert.Equal(16, pairs.Count);
+    }
+
+    [Fact]
+    public void FindTriples_WithSampleData_ShouldFind12()
+    {
+        Solver solver = new();
+
+        (List<string> computers, Dictionary<string, HashSet<string>> pairs) parsed =
+            solver.ParseData(data);
+        List<string> comps = parsed.computers;
+        Dictionary<string, HashSet<string>> pairs = parsed.pairs;
+
+        List<List<string>> trips = solver.GetTrips(comps, pairs);
+
+        Assert.Equal(12, trips.Count);
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe7()
+    {
+        Solver solver = new();
+
+        long total = long.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(7, total);
     }
 
     [Fact]
@@ -29,6 +62,37 @@ public class Day23Tests
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn
 """;
 }
