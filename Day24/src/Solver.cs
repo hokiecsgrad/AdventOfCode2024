2e3316b8cfd700a5395e7ab7488da171457a276e
Ryan Hagan <ryan@ryanhagan.net>
2024-12-25 09:38:40 -0500

    Day 24 solved part 1

--- Day24/src/Solver.cs
+++ Day24/src/Solver.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.RegularExpressions;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Day24;
@@ -6,11 +8,104 @@ public class Solver
 {
     public string SolvePart1(string[] data)
     {
-        return String.Empty;
+        long total = 0;
+
+        Dictionary<string, int> wires = GetAllWires(data);
+        SetInitialValues(ref wires, data);
+
+        int start = 0;
+        while (data[start] != "") start++;
+        start++;
+
+        while (wires.Where(w => w.Key.StartsWith("z")).Any(w => w.Value == -1))
+            for (int i = start; i < data.Length; i++)
+                CalculateResult(data[i], ref wires);
+
+        total = CalculateValueFromBinary(wires);
+
+        return total.ToString();
     }
 
     public string SolvePart2(string[] data)
     {
         return String.Empty;
+    }
+
+    public long CalculateValueFromBinary(Dictionary<string, int> wires)
+    {
+        List<string> binary = wires.Select(w => w.Key).Where(w => w.StartsWith("z")).OrderByDescending(w => w).ToList();
+        StringBuilder binVal = new();
+        foreach (string register in binary)
+            binVal.Append(wires[register].ToString());
+
+        return Convert.ToInt64(binVal.ToString(), 2);
+    }
+
+    public void CalculateResult(string operation, ref Dictionary<string, int> wires)
+    {
+        (string, string, string, string) values = BreakoutOperation(operation);
+        string left = values.Item1;
+        string op = values.Item2;
+        string right = values.Item3;
+        string result = values.Item4;
+
+        if (wires[left] == -1 || wires[right] == -1) return;
+
+        wires[result] = op switch
+        {
+            "AND" => wires[left] & wires[right],
+            "OR" => wires[left] | wires[right],
+            "XOR" => wires[left] ^ wires[right],
+            _ => throw new InvalidOperationException("Unknown operation")
+        };
+    }
+
+    public void SetInitialValues(ref Dictionary<string, int> wires, string[] data)
+    {
+        Regex initVal = new Regex(@"^([\w]{3}): ([10])$");
+
+        int i = 0;
+        while (data[i] != "")
+        {
+            var matches = initVal.Match(data[i]);
+            wires[matches.Groups[1].Value] =
+                int.Parse(matches.Groups[2].Value);
+            i++;
+        }
+    }
+
+    public Dictionary<string, int> GetAllWires(string[] data)
+    {
+        Dictionary<string, int> wires = new();
+
+        int i = 0;
+        while (data[i] != "") i++;
+        i++;
+
+        for (; i < data.Length; i++)
+        {
+            (string, string, string, string) values = BreakoutOperation(data[i]);
+
+            if (!wires.ContainsKey(values.Item1))
+                wires.Add(values.Item1, -1);
+            if (!wires.ContainsKey(values.Item3))
+                wires.Add(values.Item3, -1);
+            if (!wires.ContainsKey(values.Item4))
+                wires.Add(values.Item4, -1);
+        }
+
+        return wires;
+    }
+
+    private (string, string, string, string) BreakoutOperation(string op)
+    {
+        Regex pattern = new Regex(@"^([\w]{3}) (OR|AND|XOR) ([\w]{3}) -> ([\w]{3})$");
+        var matches = pattern.Match(op);
+        string first = matches.Groups[1].Value;
+        string oper = matches.Groups[2].Value;
+        string second = matches.Groups[3].Value;
+        string third = matches.Groups[4].Value;
+
+        return (first, oper, second, third);
     }
 }
--- Day24/tst/Day24Tests.cs
+++ Day24/tst/Day24Tests.cs
@@ -10,15 +10,65 @@ public class Day24Tests
     {
         data = sampleInput.Split(
             '\n',
-            StringSplitOptions.TrimEntries |
-            StringSplitOptions.RemoveEmptyEntries
+            StringSplitOptions.TrimEntries
+            );
+        dataSimp = simpleInput.Split(
+            '\n',
+            StringSplitOptions.TrimEntries
             );
     }
 
     [Fact]
-    public void Part1_WithSampleData_ShouldBeXXX()
+    public void ParseData_WithSampleData_ShouldWork()
     {
-        throw new NotImplementedException();
+        Solver solver = new();
+
+        Dictionary<string, int> wires = solver.GetAllWires(data);
+        solver.SetInitialValues(ref wires, data);
+
+        Assert.Equal(46, wires.Count);
+        Assert.Equal(1, wires["x03"]);
+    }
+
+    [Fact]
+    public void PerformOperation_WithSampleData_ShouldWork()
+    {
+        Solver solver = new();
+
+        Dictionary<string, int> wires = solver.GetAllWires(data);
+        solver.SetInitialValues(ref wires, data);
+
+        string op = "y02 OR x01 -> tnw";
+        solver.CalculateResult(op, ref wires);
+        Assert.Equal(1, wires["tnw"]);
+
+        op = "y02 AND x01 -> tnw";
+        solver.CalculateResult(op, ref wires);
+        Assert.Equal(0, wires["tnw"]);
+
+        op = "y02 XOR x01 -> tnw";
+        solver.CalculateResult(op, ref wires);
+        Assert.Equal(1, wires["tnw"]);
+    }
+
+    [Fact]
+    public void Part1_WithSimpleData_ShouldBe4()
+    {
+        Solver solver = new();
+
+        long total = long.Parse(solver.SolvePart1(dataSimp));
+
+        Assert.Equal(4, total);
+    }
+
+    [Fact]
+    public void Part1_WithSampleData_ShouldBe2024()
+    {
+        Solver solver = new();
+
+        long total = long.Parse(solver.SolvePart1(data));
+
+        Assert.Equal(2024, total);
     }
 
     [Fact]
@@ -29,6 +79,65 @@ public class Day24Tests
 
     private string[] data;
     private string sampleInput = """
-SAMPLE INPUT HERE
+x00: 1
+x01: 0
+x02: 1
+x03: 1
+x04: 0
+y00: 1
+y01: 1
+y02: 1
+y03: 1
+y04: 1
+
+ntg XOR fgs -> mjb
+y02 OR x01 -> tnw
+kwq OR kpj -> z05
+x00 OR x03 -> fst
+tgd XOR rvg -> z01
+vdt OR tnw -> bfw
+bfw AND frj -> z10
+ffh OR nrd -> bqk
+y00 AND y03 -> djm
+y03 OR y00 -> psh
+bqk OR frj -> z08
+tnw OR fst -> frj
+gnj AND tgd -> z11
+bfw XOR mjb -> z00
+x03 OR x00 -> vdt
+gnj AND wpb -> z02
+x04 AND y00 -> kjc
+djm OR pbm -> qhw
+nrd AND vdt -> hwm
+kjc AND fst -> rvg
+y04 OR y02 -> fgs
+y01 AND x02 -> pbm
+ntg OR kjc -> kwq
+psh XOR fgs -> tgd
+qhw XOR tgd -> z09
+pbm OR djm -> kpj
+x03 XOR y03 -> ffh
+x00 XOR y04 -> ntg
+bfw OR bqk -> z06
+nrd XOR fgs -> wpb
+frj XOR qhw -> z04
+bqk OR frj -> z07
+y03 OR x01 -> nrd
+hwm AND bqk -> z03
+tgd XOR rvg -> z12
+tnw OR pbm -> gnj
+""";
+    private string[] dataSimp;
+    private string simpleInput = """
+x00: 1
+x01: 1
+x02: 1
+y00: 0
+y01: 1
+y02: 0
+
+x00 AND y00 -> z00
+x01 XOR y01 -> z01
+x02 OR y02 -> z02
 """;
 }
